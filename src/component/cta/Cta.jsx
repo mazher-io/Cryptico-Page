e48d3d32a822f8f8a009c6cb348fb160ec4cf5be
MH Code <mazheriftikhar1@gmail.com>
2025-02-14 16:06:30 +0500

    14-Feb

--- src/component/cta/Cta.jsx
+++ src/component/cta/Cta.jsx
@@ -1,8 +1,19 @@
 import "./cta.css";
+import { motion } from "framer-motion";
 
 
 const Cta = () => {
   return (
+    <motion.div
+      whileInView={{ opacity: 1, scale: 1 }}
+      initial={{ opacity: 0, scale: 0.8 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+      viewport={{ once: true }} // Prevents re-triggering when scrolling back
+    >
     <div className="cta">
       <div className="left-sec">
         <h1>Let’s start your cryptico<br/>investment now</h1>
@@ -20,6 +31,7 @@ const Cta = () => {
         
       </div>
     </div>
+    </motion.div>
   );
 };
 
