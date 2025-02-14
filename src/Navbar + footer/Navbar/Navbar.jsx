1ba69c0785b124027e92f739c015ffdf64e311dd
MH Code <mazheriftikhar1@gmail.com>
2025-02-14 16:06:57 +0500

    14-Feb

--- src/Navbar + footer/Navbar/Navbar.jsx
+++ src/Navbar + footer/Navbar/Navbar.jsx
@@ -1,40 +1,65 @@
-import './Navbar.css'
-import logo from '../../assets/logo.svg'
-import { useEffect } from 'react';
-
-
-
-
+import "./Navbar.css";
+import logo from "../../assets/logo.svg";
+import { useEffect } from "react";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
-    useEffect(() => {
-        const handleScroll = () => {
-            const navbar = document.getElementsByClassName('Navbar')[0];
-            if (window.scrollY > 50) {
-                navbar.classList.add('scrolled');
-            } else {
-                navbar.classList.remove('scrolled');
-            }
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      const navbar = document.getElementsByClassName("Navbar")[0];
+      if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    };
 
-        window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className='Navbar'>
-        <div className='nav-left'>
-            <img className='logo'src={logo} alt="" />
+    <motion.div
+      style={{
+        position: "sticky",
+        zIndex: 1000,
+        top: 15, // or whatever value you want
+      }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      initial={{ opacity: 0, scale: 0.7 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.6,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+    >
+      <div className="Navbar">
+        <div className="nav-left">
+          <img className="logo" src={logo} alt="" />
         </div>
-        <div className='nav-right'>
-            <button className='login' onClick={() => window.location.href = 'https://crypticoo.netlify.app'}>Login</button>
-            <button className='sign-up' onClick={() => window.location.href = 'https://crypticoo.netlify.app'}>Sign up</button>
+        <div className="nav-right">
+          <button
+            className="login"
+            onClick={() =>
+              (window.location.href = "https://crypticoo.netlify.app")
+            }
+          >
+            Login
+          </button>
+          <button
+            className="sign-up"
+            onClick={() =>
+              (window.location.href = "https://crypticoo.netlify.app")
+            }
+          >
+            Sign up
+          </button>
         </div>
-      
-    </div>
-  )
-}
+      </div>
+    </motion.div>
+  );
+};
 
-export default Navbar
+export default Navbar;
